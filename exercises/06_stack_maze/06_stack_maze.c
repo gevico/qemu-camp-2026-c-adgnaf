36ac2a8c5d25b4d8a46fa0b75af276c920319e1c
Fangda <fdtsaid@gmail.com>
2026-04-21 17:04:17 +0800

    add 06 solution

--- exercises/06_stack_maze/06_stack_maze.c
+++ exercises/06_stack_maze/06_stack_maze.c
@@ -13,8 +13,69 @@ int maze[MAX_ROW][MAX_COL] = {
 
 int main(void)
 {
-	// TODO: 在这里添加你的代码
-    // I AM NOT DONE
+	struct Position {
+		int row;
+		int col;
+		int next_dir;
+	};
+	struct Position stack[MAX_ROW * MAX_COL];
+	int visited[MAX_ROW][MAX_COL] = {0};
+	int direction_row[4] = {1, -1, 0, 0};
+	int direction_col[4] = {0, 0, 1, -1};
+	int top = 0;
+	int found = 0;
+
+	stack[0].row = 0;
+	stack[0].col = 0;
+	stack[0].next_dir = 0;
+	visited[0][0] = 1;
+
+	while (top >= 0) {
+		int current_row = stack[top].row;
+		int current_col = stack[top].col;
+
+		if (current_row == MAX_ROW - 1 && current_col == MAX_COL - 1) {
+			found = 1;
+			break;
+		}
+
+		if (stack[top].next_dir >= 4) {
+			top--;
+			continue;
+		}
+
+		int dir = stack[top].next_dir;
+		int next_row;
+		int next_col;
+
+		stack[top].next_dir++;
+		next_row = current_row + direction_row[dir];
+		next_col = current_col + direction_col[dir];
+
+		if (next_row < 0 || next_row >= MAX_ROW ||
+			next_col < 0 || next_col >= MAX_COL) {
+			continue;
+		}
+
+		if (maze[next_row][next_col] == 1 || visited[next_row][next_col]) {
+			continue;
+		}
+
+		top++;
+		stack[top].row = next_row;
+		stack[top].col = next_col;
+		stack[top].next_dir = 0;
+		visited[next_row][next_col] = 1;
+	}
+
+	if (!found) {
+		printf("No path!\n");
+		return 0;
+	}
+
+	for (int i = top; i >= 0; i--) {
+		printf("(%d, %d)\n", stack[i].row, stack[i].col);
+	}
 
 	return 0;
 }
